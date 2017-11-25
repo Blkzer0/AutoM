f1101096b4f6d8eab1fd28ec395c38c84ca01f95
Blkzer0 <31668190+Blkzer0@users.noreply.github.com>
2017-11-25 03:11:23 -0500

    Update networkup.sh

--- networkup.sh
+++ networkup.sh
@@ -12,4 +12,6 @@ sleep 2
 
 echo "Network should be back up"
 
-##For whatever reason your network may go down due to certain tests you may have performed or certain scripts can cause your network-manager to go down, this script will bring it back up. Just those first two lines will fix the problem, if not you can restart the network manager and if you are having complicated problems, uncomment the last lines to reload the kernel module. Be sure to add to /usr/local/bin for ease.
+##For whatever reason your network may go down due to certain tests you may have performed or certain scripts can cause your network-manager to go down. 
+##this script will bring it back up. Just those first two lines will fix the problem, if not you can restart the network manager and if you are having complicated problems, 
+##uncomment the last lines to reload the kernel module. Be sure to add to /usr/local/bin for ease.
