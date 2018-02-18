78d2c2aaef533bd5d2164ed384a2062277dccdef
Blkzer0 <31668190+Blkzer0@users.noreply.github.com>
2018-02-18 12:35:51 -0500

    Cleaned code

--- networkup.sh
+++ networkup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+##This script consists of three ways of resetting your network manager. 
+#Uncomment the one the works best for you and add it to /usr/local/bin for ease of use.
+##The current uncommented option works best.
+
 nmcli networking off
 sleep 2
 nmcli networking on
 sleep 2
+
 #sudo service network-manager restart
 
 #reload the kernel module:
 #sudo rmmod iwlwifi
 #sudo modprobe iwlwifi
-
 echo "Network should be back up"
-
-##For whatever reason your network may go down due to certain tests you may have performed or certain scripts can cause your network-manager to go down. 
-##this script will bring it back up. Just those first two lines will fix the problem, if not you can restart the network manager and if you are having complicated problems, 
-##uncomment the last lines to reload the kernel module. Be sure to add to /usr/local/bin for ease.
